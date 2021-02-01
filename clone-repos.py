338458f3e353cf935d2d73ed8bfcca2074d2ed0f
vigenere23 <lolgab1@hotmail.com>
2021-02-01 17:43:38 -0500

    fixes

--- clone-repos.py
+++ clone-repos.py
@@ -2,38 +2,38 @@ import os
 import subprocess
 
 
-BRANCH = os.getenv("BRANCH")
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+BRANCH = os.getenv('BRANCH') or 'main'
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
 
 def get_repo_url(repo_name: str) -> str:
-    return f"https://github.com/glo2003/{repo_name}/archive/{BRANCH}.zip"
+    return f'https://github.com/glo2003/{repo_name}/archive/{BRANCH}.zip'
 
 
 def execute(command: list):
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    stdout, stderr = proc.communicate()
-
-    if stderr is not None:
-        raise Exception(stderr)
-
-    return stdout
+    subprocess.run(command, shell=True, check=True)
 
 
 def get_download_repo_command(url: str, output_path: str) -> list:
-    return ['curl', '-L', '-G', '--silent', '--fail', '--show-error', '-H', f'Authorization: token {GITHUB_TOKEN}', '-o', output_path, url]
+    return f'curl -L -G --silent --fail --show-error -H "Authorization: token {GITHUB_TOKEN}" -o {output_path} {url}'
 
 
-with open("repo-names.txt", 'r') as repo_names:
+def get_unzip_command(zip_path: str):
+    return f'unzip -q -o {zip_path} -d projects'
+
+
+with open('repo-names.txt', 'r') as repo_names:
     for repo_name in repo_names.read().splitlines():
         url = get_repo_url(repo_name)
         output_path = f"{repo_name}.zip"
-        command = get_download_repo_command(url, output_path)
+        download_command = get_download_repo_command(url, output_path)
+        unzip_command = get_unzip_command(output_path)
 
         try:
             print(f"\nDownloading repo '{repo_name}/{BRANCH}'")
-            execute(command)
-            print(f"Done")
+            execute(download_command)
+            print(f"Unzipping {output_path} to ./projects")
+            execute(unzip_command)
+            print('Done')
         except Exception as e:
-            print(f"Could not get repo '{repo_name}/{BRANCH}'")
-            print(e)
+            pass
