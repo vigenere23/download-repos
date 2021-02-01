48f7535b1271c9b99c7145cf4accdc421476b58f
vigenere23 <lolgab1@hotmail.com>
2021-02-01 02:48:40 -0500

    moved auth to personal access token - github token only works for current repo

--- clone-repos.py
+++ clone-repos.py
@@ -6,7 +6,7 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 
 def get_url(repo_name):
-    return f"https://{GITHUB_TOKEN}@github.com/glo2003/{repo_name}/archive/{BRANCH}.zip"
+    return f"https://github.com/glo2003/{repo_name}/archive/{BRANCH}.zip"
 
 
 with open("repo-names.txt", 'r') as repo_names:
@@ -15,6 +15,6 @@ with open("repo-names.txt", 'r') as repo_names:
         output_path = f"{repo_name}.zip"
 
         try:
-            os.system(f"wget -L -O {output_path} {url} || rm -f {output_path}")
+            os.system(f"wget --header \"Authorization: token {GITHUB_TOKEN}\" -L -O {output_path} {url} || rm -f {output_path}")
         except Exception:
             pass
