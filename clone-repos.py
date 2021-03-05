89e42f6e2600d4c5329c52932824a0f74703ad7d
vigenere23 <lolgab1@hotmail.com>
2021-03-05 15:31:04 -0500

    new repo list - not completed

--- clone-repos.py
+++ clone-repos.py
@@ -10,6 +10,14 @@ def get_repo_url(repo_name: str) -> str:
     return f'https://github.com/glo2003/{repo_name}/archive/{BRANCH}.zip'
 
 
+def get_download_link(repo_url: str) -> str:
+    return f'https://github.com/{repo_url}/archive/{BRANCH}.zip'
+
+
+def get_org_name(repo_name: str) -> str:
+    return repo_name.split('/')[0]
+
+
 def execute(command: list):
     subprocess.run(command, shell=True, check=True)
 
@@ -22,12 +30,29 @@ def get_unzip_command(zip_path: str):
     return f'unzip -q -o {zip_path} -d projects'
 
 
-with open('repo-names.txt', 'r') as repo_names:
+# with open('repo-names.txt', 'r') as repo_names:
+#     for repo_name in repo_names.read().splitlines():
+#         url = get_repo_url(repo_name)
+#         output_path = f"{repo_name}.zip"
+#         download_command = get_download_repo_command(url, output_path)
+#         unzip_command = get_unzip_command(output_path)
+
+#         try:
+#             print(f"\nDownloading repo '{repo_name}/{BRANCH}'")
+#             execute(download_command)
+#             # print(f"Unzipping {output_path} to ./projects")
+#             # execute(unzip_command)
+#             print('Done')
+#         except Exception as e:
+#             pass
+
+with open('repo-urls.txt', 'r') as repo_names:
     for repo_name in repo_names.read().splitlines():
-        url = get_repo_url(repo_name)
-        output_path = f"{repo_name}.zip"
+        url = get_download_link(repo_name)
+        org = get_org_name(repo_name)
+        output_path = f"{org}.zip"
         download_command = get_download_repo_command(url, output_path)
-        unzip_command = get_unzip_command(output_path)
+        # unzip_command = get_unzip_command(output_path)
 
         try:
             print(f"\nDownloading repo '{repo_name}/{BRANCH}'")
