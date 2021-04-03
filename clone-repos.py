2f81a53e781f185b2a65d1a59c5f826f94a81afc
vigenere23 <lolgab1@hotmail.com>
2021-04-02 22:09:52 -0400

    deleted old version

--- clone-repos.py
+++ clone-repos.py
@@ -30,22 +30,6 @@ def get_unzip_command(zip_path: str):
     return f'unzip -q -o {zip_path} -d projects'
 
 
-# with open('repo-names.txt', 'r') as repo_names:
-#     for repo_name in repo_names.read().splitlines():
-#         url = get_repo_url(repo_name)
-#         output_path = f"{repo_name}.zip"
-#         download_command = get_download_repo_command(url, output_path)
-#         unzip_command = get_unzip_command(output_path)
-
-#         try:
-#             print(f"\nDownloading repo '{repo_name}/{BRANCH}'")
-#             execute(download_command)
-#             # print(f"Unzipping {output_path} to ./projects")
-#             # execute(unzip_command)
-#             print('Done')
-#         except Exception as e:
-#             pass
-
 with open('repo-urls.txt', 'r') as repo_names:
     for repo_name in repo_names.read().splitlines():
         url = get_download_link(repo_name)
