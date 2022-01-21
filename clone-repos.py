a7cf0111d3536f1048f16642a5a3bd46609e3875
vigenere23 <lolgab1@hotmail.com>
2022-01-21 12:42:04 -0500

    fixed errors

--- clone-repos.py
+++ clone-repos.py
@@ -1,13 +1,18 @@
 import os
 import subprocess
+from pathlib import Path
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument('-b --branch', metavar='<branch or tag>', help='Branch to clone', default='main')
+parser.add_argument('-b', '--branch', metavar='<branch or tag>', help='Branch to clone', default='main')
 args = parser.parse_args()
 
 
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_TOKEN = os.getenv('CLONE_REPOS_GITHUB_TOKEN')
+
+
+if not GITHUB_TOKEN:
+    raise ValueError("Missing CLONE_REPOS_GITHUB_TOKEN environment variable.")
 
 
 def get_repo_url(repo_name: str) -> str:
@@ -27,26 +32,37 @@ def execute(command: list):
 
 
 def get_download_repo_command(url: str, output_path: str) -> list:
-    return f'curl -L -G --silent --fail --show-error -H "Authorization: token {GITHUB_TOKEN}" -o {output_path} {url}'
+    return f"curl --location --fail --silent --show-error --output {output_path} --header 'Authorization: token {GITHUB_TOKEN}' {url}"
 
 
 def get_unzip_command(zip_path: str):
     return f'unzip -q -o {zip_path} -d projects'
 
 
-with open('repo-urls.txt', 'r') as repo_names:
-    for repo_name in repo_names.read().splitlines():
-        url = get_download_link(repo_name)
-        org = get_org_name(repo_name)
-        output_path = f"{org}.zip"
-        download_command = get_download_repo_command(url, output_path)
-        # unzip_command = get_unzip_command(output_path)
+def main():
+    output_dir = os.path.join(Path.cwd(), 'projects')
+    os.makedirs(output_dir, exist_ok=True)
 
-        try:
-            print(f"\nDownloading repo '{repo_name}/{args.branch}'")
-            execute(download_command)
-            # print(f"Unzipping {output_path} to ./projects")
-            # execute(unzip_command)
-            print('Done')
-        except Exception as e:
-            pass
+    print(f'Downloading repos from branch {args.branch}')
+
+    with open('repo-urls.txt', 'r') as repo_names:
+        for repo_name in repo_names.read().splitlines():
+            url = get_download_link(repo_name)
+            org = get_org_name(repo_name)
+            output_path = os.path.join(output_dir, f"{org}.zip")
+            
+            download_command = get_download_repo_command(url, output_path)
+            # unzip_command = get_unzip_command(output_path)
+
+            try:
+                print(f"\nDownloading repo '{repo_name}'")
+                execute(download_command)
+                # print(f"Unzipping {output_path} to ./projects")
+                # execute(unzip_command)
+                print('Done')
+            except Exception as e:
+                print(f'Could not download this repo')
+                pass
+
+if __name__ == '__main__':
+    main()
